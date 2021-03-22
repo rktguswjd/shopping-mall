800696bd1f3982232733c1becb68db3b5e2e171e
wonseok <wonseok5893@naver.com>
2021-03-22 20:37:46 +0900

    [ADD] MemberTest - 회원 생성 테스트

---
+++ server/shoppingmall/src/test/java/com/project/shoppingmall/domain/MemberTest.java
@@ -0,0 +1,31 @@
+package com.project.shoppingmall.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void 회원생성() {
+        Member member = Member.builder()
+                .name("최원석")
+                .email("wonsok@naver.com")
+                .password("1234")
+                .phone("01041105893")
+                .build();
+        em.persist(member);
+    }
+}
