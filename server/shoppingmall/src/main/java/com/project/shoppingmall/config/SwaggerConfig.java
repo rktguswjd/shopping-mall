a580eb9fad923a1482182f49fb043f7ed19cd3b0
wonseok <wonseok5893@naver.com>
2021-04-08 21:20:10 +0900

    [ADD] Swagger SwaggerConfig - 스웨거 설정

---
+++ server/shoppingmall/src/main/java/com/project/shoppingmall/config/SwaggerConfig.java
@@ -0,0 +1,85 @@
+package com.project.shoppingmall.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private String version;
+    private String title;
+
+    @Bean
+    public Docket apiV1() {
+        version = "V1";
+        title = "Shopping-Mall API " + version;
+
+        List<ResponseMessage> responseMessages = new ArrayList<>();
+        responseMessages.add(new ResponseMessageBuilder()
+                .code(200)
+                .message("OK ~~")
+                .build());
+        responseMessages.add(new ResponseMessageBuilder()
+                .code(404)
+                .message("Not Found ~~")
+                .build());
+        responseMessages.add(new ResponseMessageBuilder()
+                .code(500)
+                .message("Internal Server Error ~~")
+                .build());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .groupName(version)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.project.shoppingmall.controller"))
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .apiInfo(apiInfo(title, version))
+                .globalResponseMessage(RequestMethod.GET, responseMessages);
+
+    }
+
+//    @Bean
+//    public Docket apiV2() {
+//        version = "V2";
+//        title = "Shopping-Mall API " + version;
+//
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .useDefaultResponseMessages(false)
+//                .groupName(version)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.project.shoppingmall.controller"))
+//                .paths(PathSelectors.ant("/**"))
+//                .build()
+//                .apiInfo(apiInfo(title, version));
+//
+//    }
+
+    private ApiInfo apiInfo(String title, String version) {
+        return new ApiInfo(
+                title,
+                "Swagger로 생성한 API Docs",
+                version,
+                "www.example.com",
+                new Contact("Contact Me", "www.example.com", "shopping@example.com"),
+                "Licenses",
+
+                "www.example.com",
+
+                new ArrayList<>());
+    }
+}
