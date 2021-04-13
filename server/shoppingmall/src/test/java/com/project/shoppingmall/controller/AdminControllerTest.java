66f9cc3adc2ff75ca7ce24439035d26315199304
wonseok <wonseok5893@naver.com>
2021-04-13 18:54:53 +0900

    [ADD] 관리자 컨트롤러 카테고리 등록 테스트 / 실패 테스트

---
+++ server/shoppingmall/src/test/java/com/project/shoppingmall/controller/AdminControllerTest.java
@@ -0,0 +1,50 @@
+package com.project.shoppingmall.controller;
+
+import com.project.shoppingmall.controller.requestdto.category.RequestCategoryEnrollInfo;
+import com.project.shoppingmall.exception.DuplicationCategoryNameException;
+import com.project.shoppingmall.service.AdminService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Transactional
+class AdminControllerTest {
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private AdminService adminService;
+
+    @Test
+    void 카테고리_등록() {
+        RequestCategoryEnrollInfo enrollInfo = new RequestCategoryEnrollInfo();
+        enrollInfo.setName("Outer");
+        adminService.enrollCategory(enrollInfo);
+
+        boolean existCategory = adminService.isDuplicatedCategory("Outer");
+
+        assertTrue(existCategory);
+    }
+
+    @Test
+    void 카테고리_존재시_실패() {
+        RequestCategoryEnrollInfo enrollInfo = new RequestCategoryEnrollInfo();
+        enrollInfo.setName("Outer");
+        adminService.enrollCategory(enrollInfo);
+
+        assertThrows(DuplicationCategoryNameException.class, () -> adminService.enrollCategory(enrollInfo));
+    }
+}
