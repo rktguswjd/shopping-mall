afbdf267dddcd65a208fe6138df564a5e54e768f
wonseok <wonseok5893@naver.com>
2021-04-16 02:22:36 +0900

    [UPDATE] 파일 Column 수정

--- server/shoppingmall/src/main/java/com/project/shoppingmall/domain/File.java
+++ server/shoppingmall/src/main/java/com/project/shoppingmall/domain/File.java
@@ -1,23 +1,29 @@
 package com.project.shoppingmall.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 import static javax.persistence.FetchType.LAZY;
 
 @Entity
+@Builder
 @Table(name = "files")
 @Getter
+@NoArgsConstructor @AllArgsConstructor
 public class File {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private int size;
+    private Long size;
     private String fileOriginalName;
     private String filePath;
+
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "item_id")
+    @JoinColumn(name = "product_id")
     private Product product;
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
