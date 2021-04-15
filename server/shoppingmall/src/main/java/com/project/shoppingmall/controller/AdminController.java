d16af311be2a07d68f375a25af5b1d81aed4e502
wonseok <wonseok5893@naver.com>
2021-04-16 02:27:12 +0900

    [ADD] 관리자 상품 등록 Controller

--- server/shoppingmall/src/main/java/com/project/shoppingmall/controller/AdminController.java
+++ server/shoppingmall/src/main/java/com/project/shoppingmall/controller/AdminController.java
@@ -1,17 +1,22 @@
 package com.project.shoppingmall.controller;
 
 import com.project.shoppingmall.controller.requestdto.category.RequestCategoryEnrollInfo;
+import com.project.shoppingmall.controller.requestdto.product.RequestProductEnrollInfo;
+import com.project.shoppingmall.domain.Member;
 import com.project.shoppingmall.service.AdminService;
+import com.project.shoppingmall.service.ProductService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -27,4 +32,11 @@ public class AdminController {
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
+    @ApiOperation(value = "상품 등록")
+    @PostMapping("product")
+    public ResponseEntity enrollProduct(@RequestPart("images") List<MultipartFile> images, RequestProductEnrollInfo enrollInfo) throws IOException {
+        Member admin = (Member)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        adminService.enrollProduct(enrollInfo, admin, images);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
 }
